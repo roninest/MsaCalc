f468539b1612a08f7cfb096ee053bae053b3b36b
Andrey Vasiliev <foursilico@gmail.com>
2018-10-26 11:21:54 +0300

    Phi func
    Add phi funk from A&S

--- msa.h
+++ msa.h
@@ -833,6 +833,32 @@ public:
     }
 
 
+    static double phi(double x)
+    {
+        // constants
+        constexpr double a1 =  0.254829592;
+        constexpr double a2 = -0.284496736;
+        constexpr double a3 =  1.421413741;
+        constexpr double a4 = -1.453152027;
+        constexpr double a5 =  1.061405429;
+        constexpr double p  =  0.3275911;
+
+        // Save the sign of x
+        int sign = 1;
+        if (x < 0) {
+            sign = -1;
+        }
+        x = abs(x)/M_SQRT2;
+
+        // A&S formula 7.1.26
+        double t = 1.0/(1.0 + p*x);
+        double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
+
+        return 0.5*(1.0 + sign*y);
+    }
+
+
+
 protected:
     std::vector<Parameter> parameters;
     std::vector<std::vector<float>> filedump;
