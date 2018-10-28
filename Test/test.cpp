f6b68041c2644387078dd2f7fafa040ec9a608c2
Andrey Vasiliev <foursilico@gmail.com>
2018-10-28 21:44:57 +0200

    File renamed

---
+++ Test/test.cpp
@@ -0,0 +1,23 @@
+#define ENABLE_PARALEL
+#include "../msa.h"
+#include "../Gnuplot.h"
+
+using namespace std;
+
+int main() {
+    msa::MsaCalc calc;
+    cout << "Dump start" << '\n';
+    calc.dump("Test/example.csv");
+    cout << "Dump end" << '\n';
+//    cout << "Load start" << '\n';
+//    calc.load();
+//    cout << "Load end" << '\n';
+    cout << "Corr start" << '\n';
+    calc.correlation();
+    cout << "Corr end" << '\n';
+    cout << "Print start" << '\n';
+    calc.printCorr("correlation.csv");
+    calc.printMSA("msa.csv");
+    cout << "Print end" << '\n';
+//    calc.plot();
+}
